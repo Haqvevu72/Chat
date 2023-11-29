e67798a0a513a6f442595925d4a597bab04241e4
Haqve_vu <novruzluqyds20@gmail.com>
2023-11-29 03:33:26 -0800

    Add project files.

---
+++ ClientSide/ClientSide.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using System.Net.Sockets;
+#pragma warning disable
+AutoResetEvent autoResetEvent1 = new AutoResetEvent(false);
+AutoResetEvent autoResetEvent2 = new AutoResetEvent(false);
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+
+var client = new TcpClient();
+client.Connect(ip, port);
+
+
+
+var stream = client.GetStream();
+
+var binaryReader = new BinaryReader(stream);
+var binaryWriter = new BinaryWriter(stream);
+
+var Mes = String.Empty;
+
+Task.Run(async () =>
+{
+    
+    Console.Write("Your Name: ");
+    var name = Console.ReadLine();
+    binaryWriter.Write(name);
+
+    while (true)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write($"To Who , {name}: ");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        var ToWho = Console.ReadLine();
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write($"Message: ");
+        Console.ForegroundColor = ConsoleColor.White;
+
+        var Message = Console.ReadLine();
+        binaryWriter.Write(ToWho + " " + Message);
+
+        Mes = binaryReader.ReadString();
+        if (Mes != String.Empty)
+        {
+            autoResetEvent1.Set();
+            autoResetEvent2.WaitOne();
+        }
+    }
+    
+});
+
+while (true)
+{
+    autoResetEvent1.WaitOne();
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write("\nServer: ");
+    Console.ForegroundColor = ConsoleColor.White;
+
+    Console.WriteLine(Mes);
+    autoResetEvent2.Set();
+
+
+}
+
+
+
+
+
+
+
---
+++ ServerSide/ServerSide.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+#pragma warning disable
+Dictionary<string,EndPoint> Clients = new Dictionary<string,EndPoint>();
+List<TcpClient> TCPclients = new List<TcpClient>();
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001;
+
+var listener = new TcpListener(ip, port);
+
+
+listener.Start(2);
+
+
+while (true)
+{
+    Task.Run(() =>
+    {
+        bool isFirstTime = true;
+
+        var client = listener.AcceptTcpClient();
+        TCPclients.Add(client); 
+        var stream = client.GetStream();
+
+        var binaryReader = new BinaryReader(stream);
+        var binaryWriter = new BinaryWriter(stream);
+
+        var clientName = String.Empty;
+        if (isFirstTime)
+        {
+            clientName = binaryReader.ReadString();
+            Console.WriteLine($"{clientName} - {client.Client.RemoteEndPoint}Connected.... ");
+
+            Clients.Add(clientName, client.Client.RemoteEndPoint);
+
+            isFirstTime = false;
+        }
+
+        while (true)
+        {
+            var Info = binaryReader.ReadString();
+            string[] array = Info.Split(' ',2);
+            var ToWho = array[0];
+            var ClientMessage = array[1];
+
+            Console.WriteLine(ToWho);
+            Console.WriteLine(ClientMessage);
+
+            bool IsFound = false;
+            for (int i = 0; i < Clients.Count; i++)
+            {
+                if (ToWho == Clients.ElementAt(i).Key)
+                {
+                    var receiver_EndPoint = Clients.ElementAt(i).Value;
+                    Console.WriteLine($"{Clients.ElementAt(i).Key} - {Clients.ElementAt(i).Value}");
+                    var receiver = TCPclients.FirstOrDefault(r => r.Client.RemoteEndPoint == receiver_EndPoint);
+
+                    var receiver_Stream = receiver.GetStream();
+
+                    var receiverWriter = new BinaryWriter(receiver_Stream);
+                    receiverWriter.Write(ClientMessage);
+                    IsFound= true;
+                    break;
+                }
+            }
+            if (IsFound == false) { binaryWriter.Write("Not Found !"); }
+            
+        }
+
+    });
+}
+
+
